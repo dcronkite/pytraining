c3f4561c270866b2467d22ff74308c3a4cbb998e
dcronkite <dcronkite@gmail.com>
2017-11-19 21:46:52 -0800

    Homeworks for week 1.

--- 001-DataStructures/homework2.py
+++ 001-DataStructures/homework2.py
@@ -1,6 +1,11 @@
 """
+Provide a word as input and then print out how many unique words have been
+entered so far.
+
 1. Count the number of unique words provided as input, and provide a report.
 2. Change all_words to a dictionary/hashmap, and count the number of times each word appears.
+3. Use the str.split() function break up white space given as input.
+    * the input "not word" would be treated as two words rather than one
 """
 
 all_words = set()
