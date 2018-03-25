06bc358096b88696052b0219dd27bd9b8feffca2
David Cronkite <dcronkite@gmail.com>
2018-03-25 15:29:49 -0700

    Added homework and example of completed

--- 008-GUI/HOMEWORK.md
+++ 008-GUI/HOMEWORK.md
@@ -1,45 +1,4 @@
 
-## Basics: Hello World ##
-
-* Open the window
-* Understand how mainloop works
-* Add a Label
-    * displays text
-    
-## Getting User Interaction ##
-
-* Introduce task
-* Entry
-* Textvariable
-* Button
-    * `command`: print some text
-* Text
-    * tk.INSERT: location of cursor
-    * tk.END: at end of text
-
-## Model-View-Controller ##
-
-* Model = data
-* View = appearance (the GUI)
-* Controller = does the work
-* Move code into class
-* Make UI elements into class attributes (^#F)
-* Add text based on entered value (with newlines)
-
-## Hook up to Program ##
-
-* Write a program to find powerpoint files
-* Parameterize path and extension
-
-## Layout Elements ##
-
-* window.title
-* window.geometry
-* window.minsize
-* pack(side=LEFT|RIGHT|TOP|BOTTOM, fill=x|y|BOTH)
-* x.grid(row, column, rowspan, columnspan)
-* sticky (north, east, south, west)
-
 ## Homework ##
 
 ### Reminders/Hints ###
--- 008-GUI/tk_gui.py
+++ 008-GUI/tk_gui.py
@@ -6,28 +6,39 @@ import os
 class SearchApp:
 
     def __init__(self, parent):
-        self.intvar = tk.IntVar()
+        self.parent = parent
+        # path variable
         self.path = tk.StringVar()
         self.path_label = tk.StringVar()
         self.path_label.set('Path')
         self.label = tk.Label(parent, textvariable=self.path_label)
-        self.label.pack()
+        # self.label.pack(side=tk.LEFT)
         self.entry = tk.Entry(parent, textvariable=self.path)
-        self.entry.pack()
+        # self.entry.pack(side=tk.LEFT)
+
+        # extension variable
         self.ext = tk.StringVar()
         self.ext_label = tk.StringVar()
         self.ext_label.set('Extension to Find')
         self.label2 = tk.Label(parent, textvariable=self.ext_label)
-        self.label2.pack()
+        # self.label2.pack()
         self.entry2 = tk.Entry(parent, textvariable=self.ext)
-        self.entry2.pack()
+        # self.entry2.pack()
         self.run_button_label = tk.StringVar()
         self.run_button_label.set('Run')
         self.button = tk.Button(parent, textvariable=self.run_button_label,
                                 command=self.find_pptx_files)
-        self.button.pack()
+        # self.button.pack()
         self.tk_text = tk.Text(parent)
-        self.tk_text.pack()
+        # self.tk_text.pack()
+
+        self.label.grid(row=1, column=0)
+        self.label2.grid(row=2, column=0)
+        self.entry.grid(row=1, column=1, columnspan=3, sticky='ew')
+        self.entry2.grid(row=2, column=1, columnspan=3, sticky='ew')
+        self.button.grid(row=4, column=1)
+        self.tk_text.grid(row=5, column=0,
+                          rowspan=6, columnspan=4)
 
     def add_text(self):
         self.tk_text.insert(tk.END, self.path.get() + '\n')
@@ -43,10 +54,15 @@ class SearchApp:
 def print_hi():
     print('Hi!!')
 
+
 if __name__ == '__main__':
     window = tk.Tk()
+    # window.geometry('500x500')
+    window.title('Search Application')
     SearchApp(window)
 
+    ## this was our original application before
+    ## moving it into the class
     # name = tk.StringVar()
     # up_button = tk.IntVar()
     # label = tk.Label(window, textvariable=name)
